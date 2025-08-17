2b65dfcc1495c409aef47105bf059486b5c987d3
R123456-123 <rishirajt781@gmail.com>
2025-08-17 16:55:04 +0530

    push3

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  let user = 'John';
+
+  return (
+    <div>
+      <h1>react + vite</h1>
+      <button>Hello I'm {user}.</button>
+    </div>
+  );
+};
+
+export default App;
